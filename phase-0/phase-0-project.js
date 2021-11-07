9715777550b11c25c8925d31a1aecbb493bdd880
Joseph Martin <josephmartin1350@gmail.com>
2021-11-07 03:59:32 -0700

    changed hover functionality

--- phase-0/phase-0-project.js
+++ phase-0/phase-0-project.js
@@ -1,8 +1,9 @@
 const button = document.querySelector('.example')
 const hover = document.querySelector('.hover')
+const popup = document.querySelector('#popup')
 
-button.addEventListener('click', () => alert('I was clicked!'))
+button.addEventListener('click', () => alert('Do you just go around clicking everything you were told to click?'))
 
-hover.addEventListener('mouseover', function() {
-    alert('Get off of me. That pointer is sharp.')
-}, {once : true});
+hover.addEventListener('mouseover', () => popup.style.display = 'block')
+
+hover.addEventListener('mouseout', () => popup.style.display = 'none')
