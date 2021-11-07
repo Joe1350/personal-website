8e9d075c14aed4d939505a4eb1dcff2ae41f2aaf
Joseph Martin <josephmartin1350@gmail.com>
2021-11-07 02:38:28 -0700

    added fucntionality to project 0 and cleaned up styling as well as added folders and html files for future projects

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const button = document.querySelector('.example')
+const hover = document.querySelector('.hover')
 
 button.addEventListener('click', () => alert('I was clicked!'))
+
+hover.addEventListener('mouseover', () => alert('Get off of me. That pointer is sharp.'))
